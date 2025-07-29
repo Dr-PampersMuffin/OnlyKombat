7af0ad1281cc0df27daa4354e96b358860d75cd2
OnlyCoinToken <pampersmuffin@gmail.com>
2025-07-29 16:21:39 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+import AdventureScene from './adventureScene.js';
 
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: 'gameCanvas',
+  backgroundColor: '#000000',
+  scene: [AdventureScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  }
+};
+
+new Phaser.Game(config);
